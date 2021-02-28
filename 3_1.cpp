c8beacdee764405d3ce88352ab5f2a11afdbbf4f
Redwanul Haque <redwanulhaque703@gmail.com>
2021-02-28 14:42:39 -0500

    Add files via upload

---
+++ 3_1.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+        int x;
+	int y;
+	cout << "Please enter a number: ";
+	cin >> x;
+	cout << "Please enter another number: ";
+	cin >> y;
+	cout << "The Sum is " << x + y <<endl;
+	cout << "The Subtraction Value is " << x - y <<endl;
+	cout << "The Product is " << x * y <<endl;
+        cout << "THe Average is " << (x + y) / 2 <<endl;	
+	return 0;
+
+}
+
