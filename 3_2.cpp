8e218efbeb81545fdedb11bb6baeadceab68cd2c
Redwanul Haque <redwanulhaque703@gmail.com>
2021-02-28 14:51:01 -0500

    Area and perimeter.cpp

---
+++ 3_2.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+	int length; 
+	int width;
+
+	cout << " Enter the length: ";
+	cin >> length;
+	cout << " Enter the width: ";
+	cin >> width;
+	cout << " The area of the rectangle is " << length * width <<endl;
+        cout << " The perimeter of the rectangle is " << (length * 2)+(2*width) <<endl;
+	return 0;	
+
+}
